2847adf1c0ad8857a2f55f68fb441962829bf92b
FND <fnd@localhost.localdomain>
2012-10-08 14:10:35 +0200

    added SPARQL push to Virtuoso adaptor
    doesn't actually seem to work yet

--- lib/iq_rdf_storage/virtuoso_adaptor.rb
+++ lib/iq_rdf_storage/virtuoso_adaptor.rb
@@ -11,28 +11,49 @@ module IqRdfStorage
     end
 
     def reset(uri)
-      res = sparql("CLEAR GRAPH <#{uri}>") # XXX: s/CLEAR/DROP/ was rejected (405)
+      res = sparql_pull("CLEAR GRAPH <#{uri}>") # XXX: s/CLEAR/DROP/ was rejected (405)
       return res == "200" # XXX: always returns 409
     end
 
-    def update(uri)
+    # uses push method if `rdf_data` is provided, pull otherwise
+    def update(uri, rdf_data=nil)
       reset(uri)
 
-      res = sparql(%{LOAD "#{uri}" INTO GRAPH <#{uri}>})
+      if rdf_data
+        res = sparql_push(uri, rdf_data.strip)
+      else
+        res = sparql_pull(%{LOAD "#{uri}" INTO GRAPH <#{uri}>})
+      end
+
       return res == "200" # XXX: always returns 409
     end
 
-    def sparql(query)
-      path = "/DAV/home/#{@username}/rdf_sink" # XXX: shouldn't this be /sparql?
-
-      uri = URI.parse("#{@host}:#{@port}#{path}")
-      req = Net::HTTP::Post.new(uri.to_s)
-      req.basic_auth(@username, @password)
-      req["Content-Type"] = "application/sparql-query"
-      req.body = query
-
-      res = Net::HTTP.new(uri.host, uri.port).request(req)
+    def sparql_push(uri, rdf_data)
+      filename = uri.gsub(/[^0-9A-Za-z]/, "_") # XXX: too simplistic?
+      path = "/DAV/home/#{@username}/rdf_sink/#{filename}"
+      res = http_request("PUT", path, rdf_data, {
+        "Expect" => "100-continue"
+      })
       return res.code
+    end
+
+    def sparql_pull(query)
+      path = "/DAV/home/#{@username}/rdf_sink" # XXX: shouldn't this be /sparql?
+      res = http_request("POST", path, query, {
+        "Content-Type" => "application/sparql-query"
+      })
+      return res.code
+    end
+
+    def http_request(method, path, body, headers={})
+      uri = URI.parse("#{@host}:#{@port}#{path}")
+
+      req = Net::HTTP.const_get(method.downcase.capitalize).new(uri.to_s)
+      req.basic_auth(@username, @password)
+      headers.each { |key, value| req[key] = value }
+      req.body = body
+
+      return Net::HTTP.new(uri.host, uri.port).request(req)
     end
 
   end
--- test/virtuoso_test.rb
+++ test/virtuoso_test.rb
@@ -8,11 +8,40 @@ class VirtuosoTest < MiniTest::Unit::TestCase
         80, "dba", "...")
   end
 
-  def test_api
+  def test_pull
     uri = "http://try.iqvoc.net/model_building.rdf"
 
     assert @adaptor.reset(uri)
     assert @adaptor.update(uri)
+  end
+
+  def test_push
+    uri = "http://try.iqvoc.net/model_building.rdf"
+
+    rdf_data = <<-EOS
+<?xml version="1.0" encoding="UTF-8"?>
+<rdf:RDF xmlns="http://try.iqvoc.net/"
+    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+    xmlns:owl="http://www.w3.org/2002/07/owl#"
+    xmlns:skos="http://www.w3.org/2004/02/skos/core#"
+    xmlns:dct="http://purl.org/dc/terms/"
+    xmlns:coll="http://try.iqvoc.net/collections/"
+    xmlns:schema="http://try.iqvoc.net/schema#">
+  <rdf:Description rdf:about="http://try.iqvoc.net/model_building">
+    <rdf:type rdf:resource="http://www.w3.org/2004/02/skos/core#Concept"/>
+    <skos:prefLabel xml:lang="en">Model building</skos:prefLabel>
+    <skos:narrower rdf:resource="http://try.iqvoc.net/model_rocketry"/>
+    <skos:narrower rdf:resource="http://try.iqvoc.net/radio-controlled_modeling"/>
+    <skos:narrower rdf:resource="http://try.iqvoc.net/scale_modeling"/>
+    <skos:broader rdf:resource="http://try.iqvoc.net/achievement_hobbies"/>
+  </rdf:Description>
+</rdf:RDF>
+    EOS
+    rdf_data.strip!
+
+    assert @adaptor.reset(uri)
+    assert @adaptor.update(uri, rdf_data)
   end
 
 end
